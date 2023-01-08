9f7d10b1ac6699e571c88480b7991f5d1556a0e6
Anatolii-melashchenko <toramulo2012@gmail.com>
2023-01-08 17:22:28 +0200

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import user from '../path/user.json'
+import Profile from './Profile/Profile.js'
+import data from '../path/data.json'
+import Statistics from './Statistics/Statistics';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
+        // display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+                  username={user.username}
+                  tag={user.tag}
+                  location={user.location}
+                  avatar={user.avatar}
+                  stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </div>
   );
 };
